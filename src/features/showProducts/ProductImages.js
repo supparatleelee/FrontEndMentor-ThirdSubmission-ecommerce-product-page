d85b9e3da8b1b0072cf851e5f29fc2044fd448f6
supparatleelee <supparat.lst@gmail.com>
2022-12-24 08:23:44 +0700

    experimented with adding another component when hovering on one component(still have a bug) :fire:

--- src/features/showProducts/ProductImages.js
+++ src/features/showProducts/ProductImages.js
@@ -2,12 +2,18 @@
 // import 'swiper/css';
 // import 'swiper/css/pagination';
 // import { Pagination } from 'swiper';
+import { useState } from 'react';
 import previewImage1 from '../../assets/images/image-product-1.jpg';
 import previewImage2 from '../../assets/images/image-product-2.jpg';
 import previewImage3 from '../../assets/images/image-product-3.jpg';
 import previewImage4 from '../../assets/images/image-product-4.jpg';
 
 function ProductImages() {
+  const [isHover, setIsHover] = useState(false);
+  const [translateClassName, setTrabslateClassName] = useState('');
+  console.log(isHover);
+  console.log(translateClassName, '2');
+
   // const pagination = {
   //   clickable: true,
   //   renderBullet: function (num) {
@@ -53,29 +59,76 @@ function ProductImages() {
         {/* box 3 - translate-x-52 */}
         {/* box 4 - translate-x-[21.7vw] */}
         <div className="block absolute bg-white w-[20%] h-[100%] rounded-[15px] bg-opacity-50 border-[#FF7D1A] border-2"></div>
+
+        {isHover ? (
+          <OnHoverImage translateClassName={translateClassName} />
+        ) : (
+          ''
+        )}
+
         <img
           src={previewImage1}
-          alt="product-preview"
+          alt="product-preview-1"
           className="rounded-[15px]"
+          onMouseEnter={() => {
+            setIsHover(true);
+            setTrabslateClassName(' ');
+          }}
+          onMouseLeave={() => {
+            setIsHover(false);
+            setTrabslateClassName('');
+          }}
         />
 
         <img
           src={previewImage2}
-          alt="product-preview"
+          alt="product-preview-2"
           className="rounded-[15px]"
+          onMouseEnter={() => {
+            setIsHover(true);
+            setTrabslateClassName('translate-x-[7.24vw]');
+          }}
+          onMouseLeave={() => {
+            setIsHover(false);
+            setTrabslateClassName('');
+          }}
         />
         <img
           src={previewImage3}
-          alt="product-preview"
+          alt="product-preview-3"
           className="rounded-[15px]"
+          onMouseEnter={() => {
+            setIsHover(true);
+            setTrabslateClassName('translate-x-52');
+          }}
+          onMouseLeave={() => {
+            setIsHover(false);
+            setTrabslateClassName('');
+          }}
         />
         <img
           src={previewImage4}
-          alt="product-preview"
+          alt="product-preview-4"
           className="rounded-[15px]"
+          onMouseEnter={() => {
+            setIsHover(true);
+            setTrabslateClassName('translate-x-[21.7vw]');
+          }}
+          onMouseLeave={() => {
+            setIsHover(false);
+            setTrabslateClassName('');
+          }}
         />
       </div>
     </div>
+  );
+}
+
+function OnHoverImage({ translateClassName }) {
+  return (
+    <div
+      className={`block absolute bg-white w-[20%] h-[100%] rounded-[15px] bg-opacity-50 ${translateClassName}`}
+    ></div>
   );
 }
 
