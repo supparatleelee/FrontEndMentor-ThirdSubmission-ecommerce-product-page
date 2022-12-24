30730f444e265426d931e5f85e0db4c07221a506
supparatleelee <supparat.lst@gmail.com>
2022-12-24 10:05:56 +0700

    feat: swiper + its navigation module style :white_check_mark:

--- src/features/showProducts/ProductImages.js
+++ src/features/showProducts/ProductImages.js
@@ -1,37 +1,27 @@
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/css';
-// import 'swiper/css/pagination';
-// import { Pagination } from 'swiper';
-import { useState } from 'react';
 import previewImage1 from '../../assets/images/image-product-1.jpg';
 import previewImage2 from '../../assets/images/image-product-2.jpg';
 import previewImage3 from '../../assets/images/image-product-3.jpg';
 import previewImage4 from '../../assets/images/image-product-4.jpg';
+import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper';
 
 function ProductImages() {
   const [isHover, setIsHover] = useState(false);
   const [translateClassName, setTrabslateClassName] = useState('');
-  console.log(isHover);
-  console.log(translateClassName, '2');
-
-  // const pagination = {
-  //   clickable: true,
-  //   renderBullet: function (num) {
-  //     return `<img src=${previewImage2} alt="product-preview" />`;
-  //   },
-  // };
 
   return (
     <div className="w-[60vw]">
-      {/* <Swiper
-        // breakpoints={}
+      <Swiper
         spaceBetween={0}
         slidesPerView={1}
-        onSlideChange={() => console.log('slide change')}
+        // onSlideChange={() => console.log('slide change')}
         // onSwiper={(swiper) => console.log(swiper)}
-        className="mySwiper"
-        pagination={pagination}
-        modules={[Pagination]}
+        className="mySwiper rounded-sm"
+        navigation={true}
+        modules={[Navigation]}
       >
         <SwiperSlide>
           <img src={previewImage1} alt="product-preview" />
@@ -45,14 +35,7 @@ function ProductImages() {
         <SwiperSlide>
           <img src={previewImage4} alt="product-preview" />
         </SwiperSlide>
-      </Swiper> */}
-
-      <img
-        src={previewImage1}
-        alt="product-preview"
-        width="100%"
-        className="rounded-[15px]"
-      />
+      </Swiper>
 
       <div className="flex mt-7 justify-between h-[79px] relative">
         {/* box 2 - translate-x-[7.24vw] */}
@@ -60,11 +43,7 @@ function ProductImages() {
         {/* box 4 - translate-x-[21.7vw] */}
         <div className="block absolute bg-white w-[20%] h-[100%] rounded-[15px] bg-opacity-50 border-[#FF7D1A] border-2"></div>
 
-        {isHover ? (
-          <OnHoverImage translateClassName={translateClassName} />
-        ) : (
-          ''
-        )}
+        {isHover && <OnHoverImage translateClassName={translateClassName} />}
 
         <img
           src={previewImage1}
