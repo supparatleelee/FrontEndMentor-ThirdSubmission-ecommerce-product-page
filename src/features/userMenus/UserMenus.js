75520dfafd21b54d7d9508bda4a814fef08eb89d
supparatleelee <supparat.lst@gmail.com>
2022-12-24 15:45:21 +0700

    feat: navbar hover behaviour

--- src/features/userMenus/UserMenus.js
+++ src/features/userMenus/UserMenus.js
@@ -4,8 +4,12 @@ import profileImage from '../../assets/images/image-avatar.png';
 function UserMenus() {
   return (
     <div className="flex gap-12 items-center">
-      <Cart />
-      <img src={profileImage} alt="Profile" width="50px" height="50px" />
+      <button>
+        <Cart />
+      </button>
+      <button>
+        <img src={profileImage} alt="Profile" width="50px" height="50px" />
+      </button>
     </div>
   );
 }
--- src/layouts/NavBar.js
+++ src/layouts/NavBar.js
@@ -1,24 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function NavBar() {
+  const [textColor, setTextColor] = useState('');
+  const [targetedItem, setTargetedItem] = useState('');
+
   return (
     <ul className="flex gap-7 text-stone-500">
-      <li>
-        <button>Collections</button>
+      <li className="relative">
+        <button
+          onMouseEnter={(e) => {
+            setTextColor('text-[#1D2025]');
+            setTargetedItem(e.target.innerText);
+          }}
+          onMouseLeave={() => {
+            setTextColor('');
+            setTargetedItem('');
+          }}
+          className={`${targetedItem === 'Collections' && textColor}`}
+        >
+          Collections
+        </button>
+        {targetedItem === 'Collections' && <HoverLine />}
       </li>
-      <li>
-        <button>Men</button>
+      <li className="relative">
+        <button
+          onMouseEnter={(e) => {
+            setTextColor('text-[#1D2025]');
+            setTargetedItem(e.target.innerText);
+          }}
+          onMouseLeave={() => {
+            setTextColor('');
+            setTargetedItem('');
+          }}
+          className={`${targetedItem === 'Men' && textColor}`}
+        >
+          Men
+        </button>
+        {targetedItem === 'Men' && <HoverLine />}
       </li>
-      <li>
-        <button>Women</button>
+      <li className="relative">
+        <button
+          onMouseEnter={(e) => {
+            setTextColor('text-[#1D2025]');
+            setTargetedItem(e.target.innerText);
+          }}
+          onMouseLeave={() => {
+            setTextColor('');
+            setTargetedItem('');
+          }}
+          className={`${targetedItem === 'Women' && textColor}`}
+        >
+          Women
+        </button>
+        {targetedItem === 'Women' && <HoverLine />}
       </li>
-      <li>
-        <button>About</button>
+      <li className="relative">
+        <button
+          onMouseEnter={(e) => {
+            setTextColor('text-[#1D2025]');
+            setTargetedItem(e.target.innerText);
+          }}
+          onMouseLeave={() => {
+            setTextColor('');
+            setTargetedItem('');
+          }}
+          className={`${targetedItem === 'About' && textColor}`}
+        >
+          About
+        </button>
+        {targetedItem === 'About' && <HoverLine />}
       </li>
-      <li>
-        <button>Contract</button>
+      <li className="relative">
+        <button
+          onMouseEnter={(e) => {
+            setTextColor('text-[#1D2025]');
+            setTargetedItem(e.target.innerText);
+          }}
+          onMouseLeave={() => {
+            setTextColor('');
+            setTargetedItem('');
+          }}
+          className={`${targetedItem === 'Contract' && textColor}`}
+        >
+          Contract
+        </button>
+        {targetedItem === 'Contract' && <HoverLine />}
       </li>
     </ul>
+  );
+}
+
+function HoverLine() {
+  return (
+    <div className="bg-[#FF7D1A] h-[4px] w-[100%] absolute translate-y-10"></div>
   );
 }
 
